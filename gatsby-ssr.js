853ffde35a1138a219848d7d8079c54b03aa6773
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-01-20 10:10:50 +0200

    removed css purger

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,3 +4,14 @@ import "typeface-merriweather"
 import "typeface-nunito"
 import "typeface-alegreya"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import React from 'react'
+//
+// export function onRenderBody(
+//   { setHeadComponents,
+//
+//   }
+// ) {
+//  setHeadComponents([
+//    <link rel="stylesheet" href="../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"/>,
+//   ])
+// }
