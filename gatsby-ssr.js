9d2d5e958e016137d540c2e7df8e21ae6f3c1fd3
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2023-01-20 09:46:57 +0200

    ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+import "typeface-nunito"
+import "typeface-alegreya"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
